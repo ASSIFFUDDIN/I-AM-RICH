c3e5c1052cc1c3c35f931f25a06cc2a1e4d3d877
SHAIKH ASSIFFUDDIN <sachinsaw@globussoft.in>
2022-09-29 15:37:50 +0530

    complete_app

---
+++ I AM RICH/I AM RICH/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I AM RICH
+//
+//  Created by GLB-311-PC on 29/09/22.
+//  Copyright © 2022 GLB-311-PC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
